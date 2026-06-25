d3d64d3a77f4a21626402fea39d53a4fc85c5175
YMeghana <23wh1a1276@bvrithyderabad.edu.in>
2026-06-25 14:54:21 +0530

    Application Notifications or API returns handled

---
+++ services/notificationService.js
@@ -0,0 +1,16 @@
+export async fetchNotifications() {
+    const response = await fetch (
+        "https://4.224.186.213/evaluation-service/notifications",
+        {
+            headers:{
+                "Authorization" : "Bearer"
+            }
+        }
+    );
+
+    if (!response.ok) {
+        throw new Error("Failed to fetch notifications");
+    }
+
+    return response.json();
+}
